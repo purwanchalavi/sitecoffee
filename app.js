bc19afd288a19152a596f2f0603db98783386547
purwanchalavi <backon153@gmail.com>
2023-01-13 19:18:38 +0545

    f commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// let btn = document.querySelector('span');
+// let div = document.querySelector('p');
+// btn.addEventListener('click',()=>{
+//    if(div.style.display === 'none'){
+//       div.style.display ='block';
+//    }
+//    else{
+//       div.style.display ='none';
+//    }
+// })
+function toggle(){
+   var x = document.getElementById("av");
+   if(x.style.display === "none"){
+      x.style.display = "block";
+   }
+   else{
+      x.style.display = "none";
+   }
+}
